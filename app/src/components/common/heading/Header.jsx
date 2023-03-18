f812650655de8cc5a95bbab6ba80f122596f0065
addpadding <tiktokaddison20@gmail.com>
2023-03-18 11:47:27 +0300

    14.3%

--- app/src/components/common/heading/Header.jsx
+++ app/src/components/common/heading/Header.jsx
@@ -3,10 +3,55 @@ import React from 'react'
 import Head from './Head'
 import "./header.css"
 
+import { Link } from 'react-router-dom'
+
 const Header = () => {
     return (
         <>
             <Head />
+
+            <header>
+
+                <nav className='flexSB'>
+                    <ul>
+
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/courses">All Courses</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/team">Team</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/pricing">pricing</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/journal">Journal</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+
+                    </ul>
+
+                    <div className="start">
+                        <div className="button">Get Certificate</div>
+                    </div>
+
+                </nav>
+
+            </header>
         </>
     )
 }
