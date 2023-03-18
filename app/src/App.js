71054c7cd0a87b1a8a46725910d75f4b19689312
addpadding <tiktokaddison20@gmail.com>
2023-03-18 09:01:10 +0300

    11.8%

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import "./App.css"
+
+import Header from './components/common/heading/Header'
+
+import {
+    BrowserRouter as Router,
+    Switch
+} from "react-router-dom";
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello React</h1>
-        </div>
+        <>
+
+
+            <Router>
+
+                <Header />
+
+                <Switch>
+
+                    {/* <Route path="/about">
+                        <About />
+                    </Route> */}
+
+                </Switch>
+            </Router>
+
+        </>
     )
 }
 
---
+++ app/src/components/common/heading/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Head from './Head'
+import "./header.css"
+
+const Header = () => {
+    return (
+        <>
+            <Head />
+        </>
+    )
+}
+
+export default Header
