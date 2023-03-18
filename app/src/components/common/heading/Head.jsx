3ebaa61b3ba0264a6be6e02a886eea68adfe350b
addpadding <tiktokaddison20@gmail.com>
2023-03-18 06:40:07 +0300

    8.2%

---
+++ app/src/components/common/heading/Head.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Head = () => {
+    return (
+        <div>
+
+            <section className='head'>
+                <div className="container flexSB">
+
+                    <div className="logo">
+
+                    </div>
+
+                </div>
+            </section>
+
+        </div>
+    )
+}
+
+export default Head
