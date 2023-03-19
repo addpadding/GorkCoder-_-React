e208e1bbfc562cbbfd304178714761c5e486cda3
addpadding <tiktokaddison20@gmail.com>
2023-03-19 13:49:27 +0300

    31.1%

--- app/src/App.js
+++ app/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/common/heading/Header'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 // ===========================
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
                 <Switch>
 
                     <Route path="/" exact component={Home} />
+                    <Route path="/about" exact component={About} />
 
                 </Switch>
             </Router>
--- app/src/components/home/hero/Hero.jsx
+++ app/src/components/home/hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./hero.css";
-import Title from "../../common/heading/title/Title";
+import Title from "../../common/title/Title";
 
 const Hero = () => {
     return (
@@ -32,6 +32,7 @@ const Hero = () => {
     </section>
 
     <div className="margin"></div>
+    
     </>
     );
 };
